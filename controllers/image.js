c373c371689b3394e296a33c06570e2ec6a3e71d
Muhammad Aktharuzzaman <akth8r@hotmail.com>
2018-07-27 13:15:22 +0100

    Added some validation checks and moved the API key from the frontend to server side for security

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'fd0bbaf654eb4b87a3361eafc4012a15'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('API error'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +25,7 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage //ES6 allows handleImage: handleImage === handleImage within the object
+    handleImage,
+    handleApiCall
 };
+//ES6 allows handleImage: handleImage === handleImage within the object
--- server.js
+++ server.js
@@ -37,7 +37,9 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
 
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(4000, () => {
     console.log('app is running on port 4000');
